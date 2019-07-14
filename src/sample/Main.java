79ba43ca66f68d2609208eb1182b51ca18ae56c2
Nicholas Fisher <nicholasfisher@bellsouth.net>
2019-07-14 12:10:56 +0200

    Removed the old MainScreenControllers and renamed MainScreenController2 to MainScreenController

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainScreen2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
         primaryStage.setTitle("Fisher");
         primaryStage.setScene(new Scene(root, 650, 500));
         primaryStage.show();
--- src/sample/MainScreenController.java
+++ src/sample/MainScreenController.java
@@ -1,55 +1,104 @@
 package sample;
-        import backend.ImageAsset;
-        import javafx.fxml.FXML;
-        import javafx.fxml.FXMLLoader;
-        import javafx.fxml.Initializable;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.control.Button;
-        import javafx.scene.control.MenuButton;
-        import javafx.scene.image.ImageView;
 
-        import javafx.event.ActionEvent;
-        import javafx.scene.control.MenuItem;
-        import javafx.scene.image.Image;
-        import javafx.stage.Stage;
+import backend.ImageAsset;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 
-        import backend.DAMController;
-        import backend.Asset;
+import backend.DAMController;
+import backend.Asset;
+import javafx.stage.Stage;
 
-        import java.net.URL;
-        import java.util.ResourceBundle;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class MainScreenController implements Initializable{
 
-public class MainScreenController implements Initializable {
-
-    ViewAssetScreenController assetScreen = new ViewAssetScreenController();
-
-    MenuItem menuItem1 = new MenuItem("Tag");
-    MenuItem menuItem2 = new MenuItem ("View");
-    MenuButton menuButton = new MenuButton("options",null ,menuItem1,menuItem2);
     DAMController dam = new DAMController();
+    ObservableList <String> listView = FXCollections.observableArrayList();
 
     @FXML
     private Button uploadBtn;
 
     @FXML
-    private MenuButton hammer_menu_button;
+    private ImageView imageView;
 
     @FXML
-    private ImageView main_screen_hammer;
+    private ListView assetList;
 
     @FXML
-    private MenuButton saw_menu_btn;
+    void uploadClicked(ActionEvent event) {
+        FileChooser fc = new FileChooser();
+        //fc.setInitialDirectory("");
+        File selectedFile = fc.showOpenDialog(null);
+        if(selectedFile != null){
+            Image img = new Image(selectedFile.toURI().toString());
+            String name = selectedFile.getName();
+            addItem(name, img);
+        }else{
+            System.out.print("File is not valid");
+        }
+    }
 
-    @FXML
-    private MenuButton hammering_nail_menu_btn;
+    public void addItem(String string, Image pic){
+        Image img = pic;
+        String name = string;
+        Asset asset = new Asset(img,name);
+        ImageView imgView = new ImageView(img);
+        Text text = new Text(name);
+        dam.uploadAsset2(asset);
+        imgView.setFitWidth(50);
+        imgView.setFitHeight(50);
+        MenuButton mb = new MenuButton();
+        mb.setText("Select option");
+        MenuItem view = new MenuItem();
+        view.setText("View");
+        view.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                openViewAssetPage(img,name);
+            }
+        });
+        MenuItem tag = new MenuItem();
+        tag.setText("Tag");
+        tag.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Asset foundAsset = dam.findAsset(name);
+                tagClicked(foundAsset);
+            }
+        });
+        mb.getItems().addAll(view,tag);
+        Pane pane1 = new Pane();
+        pane1.setPrefSize(100,50);
+        Pane pane2 = new Pane();
+        pane2.setPrefSize(100,50);
+        HBox hbox = new HBox(imgView,pane1, text,pane2,mb);
 
-    @FXML
-    private MenuButton screwdriver_menu_btn;
-
-    @FXML
-    private MenuButton wrench_menu_btn;
+        assetList.getItems().add(hbox);
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem view2 = new MenuItem();
+        contextMenu.getItems().addAll(view,tag);
+        //assetList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        //contextMenu.getAnchorLocation().equals(assetList);
+        assetList.setContextMenu(contextMenu);
+    }
 
     public void openViewAssetPage(Image pic, String name){
         FXMLLoader loader = new FXMLLoader();
@@ -57,7 +106,7 @@ public class MainScreenController implements Initializable {
         try {
             Parent root = loader.load();
             Stage stage = new Stage();
-            stage.setTitle("Hello World");
+            stage.setTitle("Fisher");
             stage.setScene(new Scene(root, 600, 500));
             stage.show();
 
@@ -78,7 +127,7 @@ public class MainScreenController implements Initializable {
             try{
                 Parent root = loader.load();
                 Stage stage = new Stage();
-                stage.setTitle("Hello World");
+                stage.setTitle("Fisher");
                 stage.setScene(new Scene(root,650,500));
                 stage.show();
 
@@ -94,7 +143,7 @@ public class MainScreenController implements Initializable {
             try {
                 Parent root = loader.load();
                 Stage stage = new Stage();
-                stage.setTitle("Hello World");
+                stage.setTitle("Fisher");
                 stage.setScene(new Scene(root, 650, 500));
                 stage.show();
 
@@ -108,121 +157,9 @@ public class MainScreenController implements Initializable {
         }
     }
 
-    void tagClickedImage(ImageAsset imageAsset){
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("tagImageScreen.fxml"));
-        try{
-            Parent root = loader.load();
-            Stage stage = new Stage();
-            stage.setTitle("Hello World");
-            stage.setScene(new Scene(root,650,500));
-            stage.show();
-
-        }catch(Exception e){
-
-        }
-        ImageAsset asset = imageAsset;
-        TagImageScreenController tagImage = loader.getController();
-        tagImage.initData(asset,dam);
-    }
-
-
-    @FXML
-    void viewHammerClicked(ActionEvent event)  {
-        String name = "hammer.jpg";
-        Asset asset = dam.findAsset(name);
-        Image pic = asset.getImg();
-        openViewAssetPage(pic,name);
-
-    }
-
-    @FXML
-    void viewScrewDriverClicked(ActionEvent event) {
-        String name = "screwdriver.jpg";
-        Asset asset = dam.findAsset(name);
-        Image pic = asset.getImg();
-        openViewAssetPage(pic,name);
-    }
-
-    @FXML
-    void viewWrenchClicked(ActionEvent event) {
-        String name = "wrench.jpg";
-        Asset asset = dam.findAsset(name);
-        Image pic = asset.getImg();
-        openViewAssetPage(pic,name);
-    }
-
-    @FXML
-    void viewSawClicked(ActionEvent event) {
-        String name = "saw.jpg";
-        Asset asset = dam.findAsset(name);
-        Image pic = asset.getImg();
-        openViewAssetPage(pic,name);
-    }
-
-    @FXML
-    void viewHammeringNailClicked(ActionEvent event) {
-        String name = "hammering_nail.jpg";
-        Asset asset = dam.findAsset(name);
-        Image pic = asset.getImg();
-        openViewAssetPage(pic,name);
-    }
-
-    @FXML
-    void tagHammerClicked(ActionEvent event) {
-        Asset asset = dam.findAsset("hammer.jpg");
-        tagClicked(asset);
-
-    }
-
-    @FXML
-    void tagScrewDriverClicked(ActionEvent event) {
-        Asset asset = dam.findAsset("screwdriver.jpg");
-        tagClicked(asset);
-    }
-
-    @FXML
-    void tagWrenchClicked(ActionEvent event) {
-        Asset asset = dam.findAsset("wrench.jpg");
-        tagClicked(asset);
-    }
-
-    @FXML
-    void tagSawClicked(ActionEvent event) {
-        Asset asset = dam.findAsset("saw.jpg");
-        tagClicked(asset);
-    }
-
-    @FXML
-    void tagHammeringNailClicked(ActionEvent event) {
-        Asset asset = dam.findAsset("hammering_nail.jpg");
-        tagClicked(asset);
-    }
-
-    @FXML
-    void uploadClicked(ActionEvent event) {
-
-    }
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-            Image hammer = new Image("sample/hammer.jpg");
-            Image screwDriver = new Image ("sample/screwdriver.jpg");
-            Image wrench = new Image("sample/wrench.jpg");
-            Image saw = new Image("sample/saw.jpg");
-            Image hammeringNail = new Image ("sample/hammering_nail.jpg");
-
-            dam.uploadAsset(hammer,"hammer.jpg");
-            dam.uploadAsset(screwDriver,"screwdriver.jpg");
-            dam.uploadAsset(wrench,"wrench.jpg");
-            dam.uploadAsset(saw,"saw.jpg");
-            dam.uploadAsset(hammeringNail,"hammering_nail.jpg");
 
     }
-
-    public void initData(DAMController dam){
-        this.dam = dam;
-    }
-
 }
+
